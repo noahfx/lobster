b838079d4fde50ba436b520131f1eb59b4c40eac
Josue Ortega <josue@debian.org>
2017-05-22 13:17:31 -0600

    Add audio downloader for youtube

---
+++ lobster/downloader.py
@@ -0,0 +1,45 @@
+import pafy
+
+from filemanager import get_workingdir
+
+class YouTube(object):
+    def __init__(self, url):
+        self.url = url
+        self._metadata(url)
+
+    def create_audiostream_dict(self, audiostreams):
+        """
+        Creates audiostream dictionary using bitrate as key
+        """
+        audiostream_dict = dict()
+        i = 0
+        for audiostream in audiostreams:
+            _key = '-'.join([audiostream.bitrate, audiostream.extension])
+            audiostream_dict[_key] = i
+            i+=1
+        return audiostream_dict
+
+    def _metadata(self, url):
+        """
+        Gets the video metadata from url
+        """
+        video = pafy.new(url)
+        self.video = video
+        self.title = video.title
+        self.duration = video.duration
+        self.audio_available = self.create_audiostream_dict(video.audiostreams)
+        self.audiostreams = video.audiostreams
+
+    def download_audio(self, audiostream_key):
+        """
+        Downloads audio file from youtube in the specified audiostream, if
+        the audiostream size/format is not specified, downloads default audio
+        """
+        audio_index = lambda : self.audio_available.get(audiostream_key)
+        tmp_dir = get_workingdir()
+        self.audiostreams[audio_index()].download(filepath=tmp_dir)
+
+"""if __name__ == '__main__':
+    yt = YouTube("https://www.youtube.com/watch?v=PYQGU6HWSXw")
+    yt.download_audio(audiostream_key='128k-m4a')
+"""
