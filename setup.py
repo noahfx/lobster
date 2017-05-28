30e79c0a924b4d24e57556b99bd17626169ef55c
Josue Ortega <josue@debian.org>
2017-05-28 17:00:58 -0600

    Adds setup.py MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+setup(
+    name="lobster",
+    version="0.1.0",
+    packages=["lobster"],
+    package_data={'': ['LICENSE']},
+    package_dir={'lobster': 'lobster'},
+    scripts=['bin/lobster'],
+    install_requires=['pafy>=0.5.2', 'pydub'],
+    author="Josue Ortega",
+    author_email="josueortega@protonmail.ch",
+    description="Split audio files into tracks with a single command",
+    long_description=open('README.md', 'rt').read(),
+    license="MIT",
+    keywords="audio track edit cut youtube ffmpeg mp3 ogg",
+    url="https://github.com/noahfx/lobster",
+    classifiers = [
+        "Development Status :: 1 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Multimedia :: Sound/Audio",
+    ],
+)
