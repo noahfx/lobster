445920062def2ae9f868788d8694e4382a138fa4
Josue Ortega <josue@debian.org>
2017-05-28 16:12:51 -0600

    Adds cmd args parser

--- lobster/lobster.py
+++ lobster/lobster.py
@@ -1,3 +1,5 @@
+import argparse
+
 from audio import create_tracks
 from downloader import YouTube
 from parser import parse_tracks_file
@@ -10,20 +12,35 @@ def get_from_youtube(source):
 def get_from_local(source):
     return source
 
-def generate_album(artist, album, tracks_file, type_, source, dest_dir,
+def generate_album(artist, album, tracks, source, input, output,
                    format='mp3'):
     """
     Generates tracks under dest_dir using the source media file (download|local)
     """
     get_media_file_src = {'youtube': get_from_youtube,
                           'local': get_from_local}
-    media_file_src = get_media_file_src.get(type_)(source)
-    audio_segments = parse_tracks_file(tracks_file)
-    create_tracks(media_file_src, dest_dir, audio_segments,
+    media_file_src = get_media_file_src.get(source)(input)
+    audio_segments = parse_tracks_file(tracks)
+    create_tracks(media_file_src, output, audio_segments,
                   artist, album, source_type=source, format=format)
 
 
-if __name__ == '__main__':
-    generate_album('Cybernetic Witchcult', 'Spaceous Cretaceous',
-                   '/tmp/tracks', 'youtube', 'https://www.youtube.com/watch?v=I_X4xJ29DOQ',
-                   '/tmp')
+parser = argparse.ArgumentParser(description='Cut audio files with a single command')
+parser.add_argument('--artist', '-ar',  type=str, required=True,
+                    help='Name of the artist of the track this will be used '\
+                    + 'to name the output directory')
+parser.add_argument('--album', '-al', type=str, required=True,
+                    help='Name of the album, this will be used to name '\
+                    + 'the output directory')
+parser.add_argument('--tracks', '-t', type=str, required=True,
+                    help='File containing the information to build the tracks')
+parser.add_argument('--source', '-s',  type=str, choices=['local', 'youtube'],
+                    required=True, help='Name of the media file source')
+parser.add_argument('--input', '-i', type=str, required=True,
+                    help='Path to the source media file')
+parser.add_argument('--output', '-o', type=str, required=True,
+                    help='Path to the utput directory')
+parser.add_argument('--format', type=str, help='Input media file format',
+                    default='mp3')
+kwargs=vars(parser.parse_args())
+generate_album(**kwargs)
