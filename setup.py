f7d697e1654e07c9150a99a930b5229f15dcba9f
Josue Ortega <josue@debian.org>
2017-12-29 10:20:46 -0600

    Bumps version to 0.3.1; updates description and tags

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup, find_packages
 setup(
     name="lobster",
-    version="0.3.0",
+    version="0.3.1",
     packages=["lobster"],
     package_data={'': ['LICENSE']},
     package_dir={'lobster': 'lobster'},
-    entry_points={'consoe_scripts' : ['lobster=lobster.__main__:main']},
+    entry_points={'console_scripts' : ['lobster=lobster.__main__:main']},
     install_requires=['pafy>=0.5.2', 'pydub'],
     author="Josue Ortega",
     author_email="josueortega@protonmail.ch",
     description="Split audio files into tracks with a single command",
     long_description=open('README.md', 'rt').read(),
     license="MIT",
-    keywords="audio track edit cut youtube ffmpeg mp3 ogg",
+    keywords="audio track edit cut youtube ffmpeg mp3 ogg split music download",
     url="https://github.com/noahfx/lobster",
     classifiers = [
         "Development Status :: 3 - Alpha",
